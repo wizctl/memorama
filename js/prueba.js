9f0570254fd91db82cd8ad895b32c678dfbb8ae6
wizctl <isen.mr@outlook.es>
2023-08-03 13:43:45 -0600

    first commit

---
+++ js/prueba.js
@@ -0,0 +1,159 @@
+let contadorTarjetas = 0;
+let PrimerTarjeta = null;
+let SegundaTarjeta = null;
+let MostrarPrimerTarjeta = null;
+let MostrarSegundaTarjeta = null;
+let PrimerResultado = null
+let SegundoResultado = null;
+let movidas = 0;
+let aciertos = 0
+
+
+
+let timer =60;
+let timerInicial =60
+let tiempo = null;
+let temporizador = false;
+
+
+let movidasEtiqueta = document.getElementById('contadorMovimientos')
+let puntajeEtiqueta = document.getElementById('contadorAciertos');
+let tiempoEtiqueta = document.getElementById('contadorTiempo');
+
+let tarjetas = [1,2,4,5,7,8,10,11,13,14,16,17];
+
+tarjetas.sort(() => Math.random()-0.5);
+document.getElementById('container-game').classList.add('animate__animated', 'animate__zoomInUp');
+
+
+const swalButtons = Swal.mixin({
+    customClass: {
+      popup: 'popup',
+      confirmButton:'btn-confirm',
+    },
+    buttonsStyling: false
+  })
+
+function contarTiempo(){
+    tiempo = setInterval(()=>{
+        timer--;
+        tiempoEtiqueta.innerHTML=timer;
+        if (timer === 0) {
+            clearInterval(tiempo);
+            bloquearTarjetas()
+            
+        }
+    },1000)
+}
+
+function bloquearTarjetas(){
+    
+    document.getElementById('container-game').classList.remove('animate__animated', 'animate__zoomInUp');
+    
+    for( let i=0; i<12;i++){
+        
+        let tarjetaBloqueada = document.getElementById(i+1);
+        tarjetaBloqueada.style.backgroundColor="#ffffff"
+        document.getElementById('container-game').classList.add('animate__animated', 'animate__fadeIn');
+        tarjetaBloqueada.innerHTML=`<img class="tapa animate__animated animate__fadeIn" src="img/Tarjetas/${tarjetas[i]}.png" alt="Tapa">`;
+        tarjetaBloqueada.disabled=true
+    }
+    swalButtons.fire({
+        width: '40%',
+        imageUrl:'../img/GIF/incorrecto_1.gif',
+        imageHeight: 500,
+        background:'linear-gradient(45deg, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6))',
+        html:
+        `<h3 class="header-text-alert"><span>!</span><span>T</span><span>i</span><span>e</span><span>m</span><span>p</span><span>o</span><span> </span><span>a</span><span>g</span><span>o</span><span>t</span><span>a</span><span>d</span><span>o</span><span>!</h3>`,
+        confirmButtonText: 'Continuar',
+        showClass: {
+            popup: 'animate__animated animate__fadeIn'
+        },
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const puntajeN1=aciertos*5;
+            const tiempoN1 =timer;
+            const movidasN1 = movidas
+            setTimeout(()=>{
+                localStorage.setItem("puntaje", puntajeN1);
+                localStorage.setItem("tiempo", tiempoN1);
+                localStorage.setItem("movidas", movidasN1);
+                window.location.href = "pruebaN2.html";
+            },1000)
+        }
+      })
+ }
+
+const destapar = (id)=>{
+    document.getElementById('container-game').classList.remove('animate__animated', 'animate__zoomInUp');
+    if (temporizador == false) {
+        contarTiempo();
+        temporizador = true;
+    }
+    contadorTarjetas++;
+
+    if (contadorTarjetas === 1) {
+
+        PrimerTarjeta = document.getElementById(`${id}`)
+        MostrarPrimerTarjeta=tarjetas[id-1]
+        PrimerTarjeta.innerHTML=  `<img class="tapa" src="img/Tarjetas/${MostrarPrimerTarjeta}.png" alt="Tapa">`
+        PrimerTarjeta.disabled=true;
+
+    } else if (contadorTarjetas === 2) {
+        SegundaTarjeta = document.getElementById(`${id}`);
+        MostrarSegundaTarjeta=tarjetas[id-1]
+        SegundaTarjeta.innerHTML= `<img class="tapa" src="img/Tarjetas/${MostrarSegundaTarjeta}.png" alt="Tapa">`
+        SegundaTarjeta.disabled=true;
+        movidas++;
+        movidasEtiqueta.innerHTML=movidas;
+        
+        if (MostrarPrimerTarjeta-1 === MostrarSegundaTarjeta || MostrarPrimerTarjeta === MostrarSegundaTarjeta-1) {
+            contadorTarjetas = 0
+            aciertos++;
+            puntajeEtiqueta.innerHTML=(aciertos * 5);
+
+            if(aciertos === 6){
+                clearInterval(tiempo)
+                tiempoEtiqueta.innerHTML = `${timer}`;
+                swalButtons.fire({
+                    width: '40%',
+                    imageUrl:'../img/GIF/Cultix-juntos.gif',
+                    imageHeight: 500,
+                    background:'linear-gradient(45deg, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6))',
+                    html:
+                    '<h3 class="header-text-alert"><span>!</span><span>E</span><span>s</span><span>p</span><span>e</span><span>c</span><span>t</span><span>a</span><span>c</span><span>u</span><span>l</span><span>a</span><span>r</span><span>!</span></h3>',
+                    confirmButtonText: 'Siguiente nivel',
+                    showClass: {
+                        popup: 'animate__animated animate__fadeIn'
+                    },
+                }).then((result) => {
+                    
+                    document.getElementById('container-game').classList.add('animate__animated', 'animate__fadeOut');
+                    if (result.isConfirmed) {
+                        const puntajeN1=aciertos*5;
+                        const tiempoN1 = timerInicial-timer;
+                        const movidasN1 = movidas
+                        setTimeout(()=>{
+                            localStorage.setItem("puntaje", puntajeN1);
+                            localStorage.setItem("tiempo", tiempoN1);
+                            localStorage.setItem("movidas", movidasN1);
+                            window.location.href = "pruebaN2.html";
+                          
+                        },1500)
+                    }
+                  })
+            }
+
+            contadorTarjetas = 0
+        } else {
+            setTimeout(()=>{
+             PrimerTarjeta.innerHTML= `<img class="tapa" src="/img/tapa2.png" alt="Tapa">`
+             PrimerTarjeta.disabled=false;
+             SegundaTarjeta.innerHTML= `<img class="tapa" src="/img/tapa2.png" alt="Tapa">`
+             SegundaTarjeta.disabled=false;
+             contadorTarjetas = 0
+            },1000)
+          }
+    }
+
+}
---
+++ js/pruebaN2.js
@@ -0,0 +1,187 @@
+let contadorTarjetas = 0;
+let PrimerTarjeta = null;
+let SegundaTarjeta = null;
+let MostrarPrimerTarjeta = null;
+let MostrarSegundaTarjeta = null;
+let PrimerResultado = null
+let SegundoResultado = null;
+let movidas = 0;
+let aciertos = 0
+
+
+
+let timer =60
+let timerInicial =60
+let tiempo = null;
+let temporizador = false;
+
+let movidasEtiqueta = document.getElementById('contadorMovimientos')
+let puntajeEtiqueta = document.getElementById('contadorAciertos');
+let tiempoEtiqueta = document.getElementById('contadorTiempo');
+
+let tarjetas = [19,20,22,23,25,26,28,29,31,32,34,35];
+
+tarjetas.sort(() => Math.random()-0.5);
+
+
+document.getElementById('container-game').classList.add('animate__animated', 'animate__fadeIn');
+
+const swalButtons = Swal.mixin({
+    customClass: {
+      popup: 'popup',
+      confirmButton:'btn-confirm',
+    },
+    buttonsStyling: false
+  })
+
+function contarTiempo(){
+    tiempo = setInterval(()=>{
+        timer--;
+        tiempoEtiqueta.innerHTML=timer;
+        if (timer === 0) {
+            clearInterval(tiempo);
+            bloquearTarjetas()
+            
+        }
+    },1000)
+}
+
+function bloquearTarjetas(){
+
+    document.getElementById('container-game').classList.remove('animate__animated', 'animate__zoomInUp');
+
+    for( let i=0; i<12;i++){
+        let tarjetaBloqueada = document.getElementById(i+1);
+        tarjetaBloqueada.style.backgroundColor="#ffffff"
+        document.getElementById('container-game').classList.add('animate__animated', 'animate__fadeIn');
+        tarjetaBloqueada.innerHTML=`<img class="tapa animate__animated animate__fadeIn" src="img/Tarjetas/${tarjetas[i]}.png" alt="Tapa">`;
+        tarjetaBloqueada.disabled=true
+    }
+    swalButtons.fire({
+        width: '40%',
+        imageUrl:'../img/GIF/incorrecto_1.gif',
+        imageHeight: 500,
+        background:'linear-gradient(45deg, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6))',
+        html:
+        `<h3 class="header-text-alert"><span>!</span><span>T</span><span>i</span><span>e</span><span>m</span><span>p</span><span>o</span><span> </span><span>a</span><span>g</span><span>o</span><span>t</span><span>a</span><span>d</span><span>o</span><span>!</h3>`,
+        confirmButtonText: 'jugar otra vez',
+        showClass: {
+            popup: 'animate__animated animate__fadeIn'
+        },
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const puntajeN1=aciertos*5;
+            const tiempoN1 =timer;
+            const movidasN1 = movidas
+            setTimeout(()=>{
+                localStorage.setItem("puntaje", puntajeN1);
+                localStorage.setItem("tiempo", tiempoN1);
+                localStorage.setItem("movidas", movidasN1);
+                window.location.href = "index.html";
+            },1000)
+        }
+      })
+ }
+
+const destapar = (id)=>{
+    document.getElementById('container-game').classList.remove('animate__animated', 'animate__zoomInUp');  
+
+    if (temporizador == false) {
+        contarTiempo();
+        temporizador = true;
+        
+    }
+    contadorTarjetas++;
+
+    if (contadorTarjetas === 1) {
+
+        
+        PrimerTarjeta = document.getElementById(`${id}`)
+        
+        MostrarPrimerTarjeta=tarjetas[id-1]
+        PrimerTarjeta.innerHTML=  `<img class="tapa" src="img/Tarjetas/${MostrarPrimerTarjeta}.png" alt="Tapa">`
+        PrimerTarjeta.disabled=true;
+        
+    } else if (contadorTarjetas === 2) {
+        SegundaTarjeta = document.getElementById(`${id}`);
+       
+        MostrarSegundaTarjeta=tarjetas[id-1]
+        SegundaTarjeta.innerHTML= `<img class="tapa" src="img/Tarjetas/${MostrarSegundaTarjeta}.png" alt="Tapa">`
+        SegundaTarjeta.disabled=true;
+        movidas++;
+        movidasEtiqueta.innerHTML=movidas;
+        if (MostrarPrimerTarjeta-1 === MostrarSegundaTarjeta || MostrarPrimerTarjeta === MostrarSegundaTarjeta-1) {
+
+                contadorTarjetas = 0
+            aciertos++;
+            puntajeEtiqueta.innerHTML=(aciertos * 5);
+
+            if(aciertos===6){
+
+                document.body.classList.remove('animate__animated', 'animate__fadeOut');
+                clearInterval(tiempo)
+                tiempoEtiqueta.innerHTML = `${timer}`;
+
+                swalButtons.fire({
+                    width: '40%',
+                    imageUrl:'../img/GIF/presentador-2.gif',
+                    imageHeight: 500,
+                    background:'linear-gradient(45deg, rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0.75))',
+                    html:
+                    '<h3 class="header-text-alert" ><span>F</span><span>e</span><span>l</span><span>i</span><span>c</span><span>i</span><span>d</span><span>a</span><span>d</span><span>e</span><span>s</span><span>!</span></h3>',
+                    confirmButtonText: 'Puntaje Final',
+                    showClass: {
+                        popup: 'animate__animated animate__fadeIn'
+                    },
+                }).then((result) => {
+                    document.getElementById('container-game').classList.add('animate__animated', 'animate__fadeOut');
+                    if (result.isConfirmed) {
+                        const movidasFinales = Number(localStorage.getItem("movidas")) + (movidas);
+                        const tiempoFinal = Number(localStorage.getItem("tiempo")) + (timerInicial-timer)
+                        const puntajeFinal = Number(localStorage.getItem("puntaje")) + (aciertos*5);
+                      
+                            
+                        swalButtons.fire({
+                            width: '40%',
+                            imageUrl:'../img/GIF/holi.gif',
+                            imageHeight: 500,
+                            background:'linear-gradient(45deg, rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0.75))',
+                            html:
+                            `<h3 class="header-text-alert" ><span>P</span><span>u</span><span>n</span><span>t</span><span>a</span><span>j</span><span>e</span><span>:</span><span> </span><span>${puntajeFinal}</span></h3>
+                            <h3 class="header-text-alert" ><span>T</span><span>i</span><span>e</span><span>m</span><span>p</span><span>o</span><span>:</span><span> </span><span>${tiempoFinal}</span><span> </span><span>s</span></h3>
+                            <h3 class="header-text-alert" ><span>M</span><span>o</span><span>v</span><span>i</span><span>d</span><span>a</span><span>s</span><span>:</span><span> </span><span>${movidasFinales}</span></h3>`,
+                            
+                            confirmButtonText: 'Jugar otra vez',
+                            showClass: {
+                                popup: 'animate__animated animate__fadeInDown'
+                            },
+                        }).then((confirm)=>{
+                            if (confirm.isConfirmed) {
+                                document.body.classList.add('animate__animated', 'animate__fadeOut');
+                                setTimeout(()=>{
+                                    window.location.href = "index.html";
+                        
+                                },1000)
+                                
+                            }
+                        })
+                       
+                    }
+                  })
+                
+            }
+                
+            
+            contadorTarjetas = 0
+        } else {
+            setTimeout(()=>{
+             PrimerTarjeta.innerHTML= `<img class="tapa" src="/img/tapa2.png" alt="Tapa">`
+             PrimerTarjeta.disabled=false;
+             SegundaTarjeta.innerHTML= `<img class="tapa" src="/img/tapa2.png" alt="Tapa">`
+             SegundaTarjeta.disabled=false;
+             contadorTarjetas = 0
+            },1000)
+          }
+    }
+
+}
